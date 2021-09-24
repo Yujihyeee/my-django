ae3c35528f05f80a139422c6954f54ddc0154fcb
Yujihyeee <rladnddl@naver.com>
2021-09-24 14:32:21 +0900

    14

--- admin/sorting/models.py
+++ admin/sorting/models.py
@@ -1,9 +1,8 @@
 from dataclasses import dataclass
-
 from django.db import models
-
-
 # Create your models here.
+
+
 class Sorting(object):
 
     def bubble_sort(self):
@@ -19,15 +18,23 @@ class Sorting(object):
 @dataclass
 class Palindrome(object):
     input_string: str
+    
+    @property
+    def input_string(self) -> str: return self._input_string
+    @input_string.setter
+    def input_string(self, input_string): self._input_string = input_string
 
-    def str_to_list(payload: str) -> []:
-        return [i for i in payload if i.isalnum()]
+    def str_to_list(self) -> []:
+        return [i for i in self.input_string if i.isalnum()]
 
-    def isPalindrome(ls: []) -> bool:
+    def isPalindrome(self) -> bool:
+        ls = self.str_to_list()
+        print(f'ls len : {len(ls)}')
         return {"RESULT": False for i in ls if ls.pop(0) != ls.pop()}
 
-    def reverse_string(self):
-        pass
+    def reverse_string(self) -> []:
+        strs = self.str_to_list()
+        return strs[::-1]
 
 
 @dataclass
--- admin/sorting/tests.py
+++ admin/sorting/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 import unittest
-from admin.sorting.models import MySum
+from admin.sorting.models import MySum, Palindrome
+
 
 class TestMySum(unittest.TestCase):
 
@@ -11,6 +12,22 @@ class TestMySum(unittest.TestCase):
         res = instance.one_to_ten_sum_2()
         print(f'My Expected Value is {res}')
         self.assertEqual(res, 55)
+
+
+class TestPalindrome(unittest, TestCase):
+
+    def test_str_to_list(self):
+        instance = Palindrome()
+        instance.input_string = ''
+        res1 = instance.isPalindrome()
+        self.assertEqual(res1, {"RESULT": False})
+
+    def test_reverse_string(self):
+        instance = Palindrome()
+        instance.input_string = ''
+        res2 = instance.reverse_string()
+        self.assertEqual(res2, [])
+
 
 if __name__ == '__main__':
     unittest.main()
